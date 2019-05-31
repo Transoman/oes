3b7650b00b482ecb1783b6597dd03cbffca2d14d
transoman <vasiag10@gmail.com>
2019-05-31 10:24:44 +0300

    Ready hero, tasks nad benefits section

--- app/static/js/common.js
+++ app/static/js/common.js
@@ -1,6 +1,9 @@
 global.jQuery = require('jquery');
 var svg4everybody = require('svg4everybody'),
-popup = require('jquery-popup-overlay');
+popup = require('jquery-popup-overlay'),
+ScrollMagic = require('scrollmagic');
+import TimelineMax from 'gsap/TimelineMax';
+import TweenMax from 'gsap/TweenMax';
 
 jQuery(document).ready(function($) {
 
@@ -18,6 +21,43 @@ jQuery(document).ready(function($) {
       $(this).find('label.error').remove();
     }
   });
+
+  var controller = new ScrollMagic.Controller();
+
+  function pathPrepare ($el) {
+    var lineLength = $el[0].getTotalLength();
+    $el.css("stroke-dasharray", lineLength);
+    $el.css("stroke-dashoffset", lineLength);
+  }
+  
+  var $word = $("#path path");
+
+  pathPrepare($word);
+
+  var pathLength = $word[0].getTotalLength();
+
+  window.addEventListener("scroll", function(e) {
+ 
+    // What % down is it? 
+    var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+      
+    // Length to offset the dashes
+    var drawLength = pathLength * scrollPercentage;
+    
+    // Draw in reverse
+    $word[0].style.strokeDashoffset = pathLength - drawLength;
+    
+  });
+
+  // var tween = new TimelineMax()
+  //   .add(TweenMax.to($word, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
+  //   .add(TweenMax.to("path", 1, {stroke: "#33629c", ease:Linear.easeNone}), 0);			// change color during the whole thing
+
+	// // build scene
+	// var scene = new ScrollMagic.Scene({triggerElement: ".process-line", duration: 200, tweenChanges: true})
+	// 				.setTween(tween)
+	// 				// .addIndicators() // add indicators (requires plugin)
+	// 				.addTo(controller);
 
   // SVG
   svg4everybody({});
