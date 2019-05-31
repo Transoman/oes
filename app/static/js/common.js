aca380beb932f8eb3fb3abe4021d9021e0213363
transoman <vasiag10@gmail.com>
2019-05-31 15:00:16 +0300

    Ready process section

--- app/static/js/common.js
+++ app/static/js/common.js
@@ -1,7 +1,7 @@
 global.jQuery = require('jquery');
 var svg4everybody = require('svg4everybody'),
-popup = require('jquery-popup-overlay'),
-ScrollMagic = require('scrollmagic');
+popup = require('jquery-popup-overlay');
+// ScrollMagic = require('scrollmagic');
 import TimelineMax from 'gsap/TimelineMax';
 import TweenMax from 'gsap/TweenMax';
 
@@ -22,51 +22,184 @@ jQuery(document).ready(function($) {
     }
   });
 
-  var controller = new ScrollMagic.Controller();
+  let scrollProcces = function() {
+    if ($(window).width() > 767) {
+      var controller = new ScrollMagic.Controller();
 
-  function pathPrepare ($el) {
-    var lineLength = $el[0].getTotalLength();
-    $el.css("stroke-dasharray", lineLength);
-    $el.css("stroke-dashoffset", lineLength);
-  }
-  
-  var $word = $("#path path");
-
-  pathPrepare($word);
-
-  var pathLength = $word[0].getTotalLength();
-
-  // window.addEventListener("scroll", function(e) {
- 
-  //   // What % down is it? 
-  //   var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-      
-  //   // Length to offset the dashes
-  //   var drawLength = pathLength * scrollPercentage;
+      function pathPrepare ($el) {
+        var lineLength = $el[0].getTotalLength();
+        $el.css("stroke-dasharray", lineLength);
+        $el.css("stroke-dashoffset", lineLength);
+      }
     
-  //   // Draw in reverse
-  //   $word[0].style.strokeDashoffset = pathLength - drawLength;
+      var $word = $("#path path");
     
-  // });
+      pathPrepare($word);
+    
+      var pathLength = $word[0].getTotalLength();
+    
+      var tween = new TimelineMax()
+        .add(TweenMax.to($word, 1, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
+    
+      // // build scene
+      let pinScene = new ScrollMagic.Scene({
+        triggerElement: ".process",
+        duration: 660,
+        triggerHook: 0,
+      })
+      .setPin('.process', {
+        pushFollowers: true
+      })
+      .addTo(controller);
+    
+      var scene = new ScrollMagic.Scene({
+        triggerElement: ".process",
+        duration: 660,
+        triggerHook: 0,
+        tweenChanges: true
+      })
+        .setTween(tween)
+        .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+    
+      scene.on("progress", function (event) {
+        if (event.progress <= 0.1428) {
+          $('.process-line__item--1').addClass('is-active');
+          $('.process-list__item--1 .process-list__title').addClass('is-active');
+    
+          TweenMax.to(".process-list__item--1 .process-list__content", 0.5, {autoAlpha:1});
+        }
+        else {
+          $('.process-line__item--1').removeClass('is-active');
+          $('.process-list__item--1 .process-list__title').removeClass('is-active');
+    
+          TweenMax.to(".process-list__item--1 .process-list__content", 0.5, {autoAlpha:0});
+        }
+    
+        if (event.progress > 0.1428 && event.progress <= 0.2856) {
+          $('.process-line__item--2').prevAll().addClass('less');
+          $('.process-line__item--2').addClass('is-active');
+          $('.process-list__item--2 .process-list__title').addClass('is-active');
+    
+          TweenMax.to(".process-list__item--2 .process-list__content", 0.5, {autoAlpha:1});
+        }
+        else {
+          $('.process-line__item--2').nextAll().removeClass('less');
+          $('.process-line__item--2').removeClass('is-active');
+          $('.process-list__item--2 .process-list__title').removeClass('is-active');
+    
+          TweenMax.to(".process-list__item--2 .process-list__content", 0.5, {autoAlpha:0});
+        }
+    
+        if (event.progress > 0.2856 && event.progress <= 0.4284) {
+          $('.process-line__item--3').prevAll().addClass('less');
+          $('.process-line__item--3').addClass('is-active');
+          $('.process-list__item--3 .process-list__title').addClass('is-active');
+    
+          TweenMax.to(".process-list__item--3 .process-list__content", 0.5, {autoAlpha:1});
+        }
+        else {
+          $('.process-line__item--3').nextAll().removeClass('less');
+          $('.process-line__item--3').removeClass('is-active');
+          $('.process-list__item--3 .process-list__title').removeClass('is-active');
+    
+          TweenMax.to(".process-list__item--3 .process-list__content", 0.5, {autoAlpha:0});
+        }
+    
+        if (event.progress > 0.4284 && event.progress <= 0.5712) {
+          $('.process-line__item--4').prevAll().addClass('less');
+          $('.process-line__item--4').addClass('is-active');
+          $('.process-list__item--4 .process-list__title').addClass('is-active');
+    
+          TweenMax.to(".process-list__item--4 .process-list__content", 0.5, {autoAlpha:1});
+        }
+        else {
+          $('.process-line__item--4').nextAll().removeClass('less');
+          $('.process-line__item--4').removeClass('is-active');
+          $('.process-list__item--4 .process-list__title').removeClass('is-active');
+    
+          TweenMax.to(".process-list__item--4 .process-list__content", 0.5, {autoAlpha:0});
+        }
+    
+        if (event.progress > 0.5712 && event.progress <= 0.7140) {
+          $('.process-line__item--5').prevAll().addClass('less');
+          $('.process-line__item--5').addClass('is-active');
+          $('.process-list__item--5 .process-list__title').addClass('is-active');
+    
+          TweenMax.to(".process-list__item--5 .process-list__content", 0.5, {autoAlpha:1});
+        }
+        else {
+          $('.process-line__item--5').nextAll().removeClass('less');
+          $('.process-line__item--5').removeClass('is-active');
+          $('.process-list__item--5 .process-list__title').removeClass('is-active');
+    
+          TweenMax.to(".process-list__item--5 .process-list__content", 0.5, {autoAlpha:0});
+        }
+    
+        if (event.progress > 0.7140 && event.progress <= 0.8568) {
+          $('.process-line__item--6').prevAll().addClass('less');
+          $('.process-line__item--6').addClass('is-active');
+          $('.process-list__item--6 .process-list__title').addClass('is-active');
+    
+          TweenMax.to(".process-list__item--6 .process-list__content", 0.5, {autoAlpha:1});
+        }
+        else {
+          $('.process-line__item--6').nextAll().removeClass('less');
+          $('.process-line__item--6').removeClass('is-active');
+          $('.process-list__item--6 .process-list__title').removeClass('is-active');
+    
+          TweenMax.to(".process-list__item--6 .process-list__content", 0.5, {autoAlpha:0});
+        }
+    
+        if (event.progress > 0.8568) {
+          $('.process-line__item--7').prevAll().addClass('less');
+          $('.process-line__item--7').addClass('is-active');
+          $('.process-list__item--7 .process-list__title').addClass('is-active');
+    
+          TweenMax.to(".process-list__item--7 .process-list__content", 0.5, {autoAlpha:1});
+        }
+        else {
+          $('.process-line__item--7').nextAll().removeClass('less');
+          $('.process-line__item--7').removeClass('is-active');
+          $('.process-list__item--7 .process-list__title').removeClass('is-active');
+    
+          TweenMax.to(".process-list__item--7 .process-list__content", 0.5, {autoAlpha:0});
+        }
+      });
+    }
+  };
 
-  // var tween = new TimelineMax()
-  //   .add(TweenMax.to($word, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
-  //   .add(TweenMax.to("path", 1, {stroke: "#33629c", ease:Linear.easeNone}), 0);			// change color during the whole thing
+  // Accordion
+  let accordion = function() {
+    let el = $('.process-list__item');
+    let elTitle = $('.process-list__title');
+    let content = $('.process-list__content');
 
-  // // build scene
-  var scene = new ScrollMagic.Scene({
-    triggerElement: ".process",
-    duration: 700,
-    // tweenChanges: true
-  })
-    .setPin('.process', {
-      pushFollowers: true
-    })
-    // .setTween(tween)
-    // .addIndicators() // add indicators (requires plugin)
-    .addTo(controller);
+    console.log($('.process-list__item')[0]);
+
+    $($('.process-list__item')[0]).addClass('active').find(content).slideDown(500);
+
+    elTitle.click(function() {
+      if ($(this).parent().hasClass('active')) {
+        $(this).parent().removeClass('active');
+        $(this).next().slideUp(500);
+      }
+      else {
+        $(this).parent().addClass('active');
+        content.not($(this).next()).slideUp(500);
+        elTitle.not($(this)).parent().removeClass('active');
+        $(this).next().slideDown(500);
+      }
+    });
+  };
 
   // SVG
   svg4everybody({});
+
+  scrollProcces();
+
+  if ($(window).width() < 768) {
+    accordion();
+  }
 
 });
