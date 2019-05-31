e2d890a110cee7ff98e08e6f3d9cd8473eaac6ae
transoman <vasiag10@gmail.com>
2019-05-31 11:30:47 +0300

    Ready form section

--- app/static/js/common.js
+++ app/static/js/common.js
@@ -36,28 +36,35 @@ jQuery(document).ready(function($) {
 
   var pathLength = $word[0].getTotalLength();
 
-  window.addEventListener("scroll", function(e) {
+  // window.addEventListener("scroll", function(e) {
  
-    // What % down is it? 
-    var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+  //   // What % down is it? 
+  //   var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
       
-    // Length to offset the dashes
-    var drawLength = pathLength * scrollPercentage;
+  //   // Length to offset the dashes
+  //   var drawLength = pathLength * scrollPercentage;
     
-    // Draw in reverse
-    $word[0].style.strokeDashoffset = pathLength - drawLength;
+  //   // Draw in reverse
+  //   $word[0].style.strokeDashoffset = pathLength - drawLength;
     
-  });
+  // });
 
   // var tween = new TimelineMax()
   //   .add(TweenMax.to($word, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
   //   .add(TweenMax.to("path", 1, {stroke: "#33629c", ease:Linear.easeNone}), 0);			// change color during the whole thing
 
-	// // build scene
-	// var scene = new ScrollMagic.Scene({triggerElement: ".process-line", duration: 200, tweenChanges: true})
-	// 				.setTween(tween)
-	// 				// .addIndicators() // add indicators (requires plugin)
-	// 				.addTo(controller);
+  // // build scene
+  var scene = new ScrollMagic.Scene({
+    triggerElement: ".process",
+    duration: 700,
+    // tweenChanges: true
+  })
+    .setPin('.process', {
+      pushFollowers: true
+    })
+    // .setTween(tween)
+    // .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
 
   // SVG
   svg4everybody({});
