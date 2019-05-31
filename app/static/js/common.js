3170cfff7cd8101de234b147eae9b2c28c4a1b77
transoman <vasiag10@gmail.com>
2019-05-31 18:02:17 +0300

    Not ready scheme section

--- app/static/js/common.js
+++ app/static/js/common.js
@@ -22,7 +22,7 @@ jQuery(document).ready(function($) {
     }
   });
 
-  let scrollProcces = function() {
+  let scrollProcess = function() {
     if ($(window).width() > 767) {
       var controller = new ScrollMagic.Controller();
 
@@ -31,16 +31,16 @@ jQuery(document).ready(function($) {
         $el.css("stroke-dasharray", lineLength);
         $el.css("stroke-dashoffset", lineLength);
       }
-    
+
       var $word = $("#path path");
-    
+
       pathPrepare($word);
-    
+
       var pathLength = $word[0].getTotalLength();
-    
+
       var tween = new TimelineMax()
-        .add(TweenMax.to($word, 1, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
-    
+        .add(TweenMax.to($word, 1, {strokeDashoffset: 0, ease:Linear.easeNone})); // draw word for 0.9
+
       // // build scene
       let pinScene = new ScrollMagic.Scene({
         triggerElement: ".process",
@@ -59,7 +59,7 @@ jQuery(document).ready(function($) {
         tweenChanges: true
       })
         .setTween(tween)
-        .addIndicators() // add indicators (requires plugin)
+        // .addIndicators() // add indicators (requires plugin)
         .addTo(controller);
     
       scene.on("progress", function (event) {
@@ -166,6 +166,105 @@ jQuery(document).ready(function($) {
           TweenMax.to(".process-list__item--7 .process-list__content", 0.5, {autoAlpha:0});
         }
       });
+
+      // Scheme
+      var schemeLine = $("#scheme-list__line path");
+
+      var tweenScheme = new TimelineMax()
+        .add(TweenMax.to(schemeLine, 1, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
+
+      pathPrepare(schemeLine);
+
+      let scene2 = new ScrollMagic.Scene({
+        triggerElement: ".scheme",
+        duration: 800,
+        triggerHook: 0,
+        tweenChanges: true
+      })
+        .setTween(tweenScheme)
+        // .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+
+        scene2.on("progress", function (event) {
+        if (event.progress <= 0.1111) {
+          $('.scheme-list__item--1').addClass('is-active');
+        }
+        else {
+          $('.scheme-list__item--1').removeClass('is-active');
+        }
+    
+        if (event.progress > 0.1111 && event.progress <= 0.2222) {
+          $('.scheme-list__item--2').prevAll().addClass('less');
+          $('.scheme-list__item--2').addClass('is-active');
+        }
+        else {
+          $('.scheme-list__item--2').nextAll().removeClass('less');
+          $('.scheme-list__item--2').removeClass('is-active');
+        }
+    
+        if (event.progress > 0.2222 && event.progress <= 0.3333) {
+          $('.scheme-list__item--3').prevAll().addClass('less');
+          $('.scheme-list__item--3').addClass('is-active');
+        }
+        else {
+          $('.scheme-list__item--3').nextAll().removeClass('less');
+          $('.scheme-list__item--3').removeClass('is-active');
+        }
+    
+        if (event.progress > 0.3333 && event.progress <= 0.4444) {
+          $('.scheme-list__item--4').prevAll().addClass('less');
+          $('.scheme-list__item--4').addClass('is-active');
+        }
+        else {
+          $('.scheme-list__item--4').nextAll().removeClass('less');
+          $('.scheme-list__item--4').removeClass('is-active');
+        }
+    
+        if (event.progress > 0.4444 && event.progress <= 0.5555) {
+          $('.scheme-list__item--5').prevAll().addClass('less');
+          $('.scheme-list__item--5').addClass('is-active');
+        }
+        else {
+          $('.scheme-list__item--5').nextAll().removeClass('less');
+          $('.scheme-list__item--5').removeClass('is-active');
+        }
+    
+        if (event.progress > 0.5555 && event.progress <= 0.6666) {
+          $('.scheme-list__item--6').prevAll().addClass('less');
+          $('.scheme-list__item--6').addClass('is-active');
+        }
+        else {
+          $('.scheme-list__item--6').nextAll().removeClass('less');
+          $('.scheme-list__item--6').removeClass('is-active');
+        }
+    
+        if (event.progress > 0.6666 && event.progress <= 0.7777) {
+          $('.scheme-list__item--7').prevAll().addClass('less');
+          $('.scheme-list__item--7').addClass('is-active');
+        }
+        else {
+          $('.scheme-list__item--7').nextAll().removeClass('less');
+          $('.scheme-list__item--7').removeClass('is-active');
+        }
+
+        if (event.progress > 0.7777 && event.progress <= 0.8888) {
+          $('.scheme-list__item--8').prevAll().addClass('less');
+          $('.scheme-list__item--8').addClass('is-active');
+        }
+        else {
+          $('.scheme-list__item--8').nextAll().removeClass('less');
+          $('.scheme-list__item--8').removeClass('is-active');
+        }
+
+        if (event.progress > 0.8888) {
+          $('.scheme-list__item--9').prevAll().addClass('less');
+          $('.scheme-list__item--9').addClass('is-active');
+        }
+        else {
+          $('.scheme-list__item--9').nextAll().removeClass('less');
+          $('.scheme-list__item--9').removeClass('is-active');
+        }
+      });
     }
   };
 
@@ -196,7 +295,7 @@ jQuery(document).ready(function($) {
   // SVG
   svg4everybody({});
 
-  scrollProcces();
+  scrollProcess();
 
   if ($(window).width() < 768) {
     accordion();
